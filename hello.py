65b65b12684b4045eeeb265b5528bafe7a4fd494
Phill Conrad <pconrad@cs.ucsb.edu>
2017-06-26 16:09:44 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return current_app.send_static_file('index.html')
+  # return current_app.send_static_file('index.html')
+  return render_template('home.html')
